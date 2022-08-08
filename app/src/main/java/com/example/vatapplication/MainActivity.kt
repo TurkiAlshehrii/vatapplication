8ba77477bf931a40a7253a5b25ddfecba1e7a649
TurkiAlshehrii <alshhryt547+1@gmail.com>
2022-08-08 12:55:45 +0300

    after edit and now we can take value from users finish the app

--- app/src/main/java/com/example/vatapplication/MainActivity.kt
+++ app/src/main/java/com/example/vatapplication/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import com.example.vatapplication.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
 class MainActivity : AppCompatActivity() {
 
     // binding object
@@ -25,6 +27,31 @@ class MainActivity : AppCompatActivity() {
     }// End onCreate
 
     private fun calculateVat() {
+        val stringVatet = binding.etTotalCost.text.toString()
+        val cost = stringVatet.toDouble()
+
+        // Radio group
+        val selectedID = binding.rgVatOption.checkedRadioButtonId
+
+        val vatPercentage = when(selectedID){
+            R.id.rb_vat_10 -> 0.10
+            R.id.rb_vat_15 -> 0.15
+            else -> 0.20
+        } // End when
+
+        var vat = vatPercentage * cost
+        var total = cost + vat
+        // Switch value
+        val roundVat = binding.switchRoundUp.isChecked
+
+        if (roundVat){
+            total = kotlin.math.ceil(total)
+        }
+
+        // Total Formatting
+        val formatedTotal = NumberFormat.getCurrencyInstance().format(total)
+        binding.txtCostTotal.text = getString(R.string.total_amount, formatedTotal)
+
     }
 
 }// End MainActivity
